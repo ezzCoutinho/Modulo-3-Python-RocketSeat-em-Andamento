1846a5025dbc6a79891267c682b6f4be8e2fd457
ezzCoutinho <lammerlopes@hotmail.com>
2024-12-31 14:54:54 -0300

    module 3

--- tests.py
+++ tests.py
@@ -8,7 +8,8 @@ tasks = []
 def test_create_task():
   new_task_data = {
     "title": "Nova tarefa.",
-    "description": "Usando pytest"
+    "description": "Usando pytest",
+    "completed": True,
   }
   response = requests.post(f"{BASE_URL}/tasks", json=new_task_data)
   assert response.status_code == 200
@@ -16,3 +17,22 @@ def test_create_task():
   assert "message" in response_json
   assert "id" in response_json
   tasks.append(response_json["id"]) # pegando uma chave de um dicionário.
+
+def test_get_tasks():
+  response = requests.get(f"{BASE_URL}/tasks")
+  assert response.status_code == 200
+  response_json = response.json()
+  assert "tasks" in response_json
+  assert "total_tasks" in response_json
+  
+def test_get_task():
+  if tasks:
+    tasks_id = tasks[0]
+    response = requests.get(f"{BASE_URL}/tasks/{tasks_id}")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert tasks_id == response_json["id"] # verificando se é o mesmo ID 0
+    assert "id" in response_json
+    assert "title" in response_json
+    assert "description" in response_json
+    assert "completed" in response_json
